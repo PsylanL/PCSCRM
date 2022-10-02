385cc97ede0d09f26a638ccbeb2f9a52ebb883ab
DanielSalazar88 <daniel_salazar82201@elpoli.edu.co>
2022-10-02 17:06:46 -0500

    (FEAT) the client interface was implemented and the client table was added

---
+++ FrontEnd/js/clients.js
@@ -0,0 +1,49 @@
+
+
+async function client() {
+    const getClients = "http://localhost:8080/api/client/list";
+    let request = await fetch(getClients);
+    let response = await request.json();
+
+    console.log(response);
+
+    let clientsTable = document.getElementById('clients');
+    let tableBody = document.getElementById('tbody');
+
+    response.forEach(element => {
+        let row = document.createElement('tr');
+        let td = document.createElement('td');
+
+        td.innerText = element.id;
+        row.appendChild(td);
+
+        td = document.createElement('td');
+        td.innerText = element.adress;
+        row.appendChild(td);
+
+        td = document.createElement('td');
+        td.innerText = element.cellPhone;
+        row.appendChild(td);
+
+        td = document.createElement('td');
+        td.innerText = element.mail;
+        row.appendChild(td);
+
+        td = document.createElement('td');
+        td.innerText = element.name;
+        row.appendChild(td);
+
+        td = document.createElement('td');
+        td.innerHTML = '<button class="icon-trash btn btn-outline-primary btn-sm" id="btnDelete"></button>'
+            +' '+ '<button class="icon-edit btn btn-outline-primary btn-sm" id="btnEdit"></button>';
+        row.appendChild(td);
+
+        tableBody.appendChild(row);
+    });
+
+    clientsTable.appendChild(tableBody);
+
+
+}
+
+window.onload = client; 
