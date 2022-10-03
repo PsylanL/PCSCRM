67151de1e14305b00ac8ed9dc7ae301af5ca74c6
psylan <dylan_lopez82201@elpoli.edu.co>
2022-10-02 20:55:31 -0500

    (FEAT) Sidebar option moved to a modal function to create clients. Manage option created

--- FrontEnd/js/clients.js
+++ FrontEnd/js/clients.js
@@ -22,7 +22,7 @@ async function client() {
         row.appendChild(td);
 
         td = document.createElement('td');
-        td.innerText = element.adress;
+        td.innerText = element.name;
         row.appendChild(td);
 
         td = document.createElement('td');
@@ -34,7 +34,7 @@ async function client() {
         row.appendChild(td);
 
         td = document.createElement('td');
-        td.innerText = element.name;
+        td.innerText = element.adress;
         row.appendChild(td);
 
         td = document.createElement('td');
@@ -50,4 +50,48 @@ async function client() {
 
 }
 
+const openModal = document.getElementById('btn-agg-client');
+const  modal1 = document.querySelector('.modal1');
+const closeModal = document.querySelector('.modal_close');
+openModal.addEventListener('click', (e)=>{ 
+    e.preventDefault();/*Evita que se ponga un # en el url cada que se abre el modal */
+    modal1.classList.add('modal1--show')
+})
+
+closeModal.addEventListener('click', (e)=>{ 
+    e.preventDefault();/*Evita que se ponga un # en el url cada que se abre el modal */
+    modal1.classList.remove('modal1--show')
+})
+
 window.onload = client; 
+
+
+/* REGISTER FUNCTION*/
+
+async function register(){
+    let user = {};
+    user.id = document.getElementById('txtId').value;
+    user.name = document.getElementById('txtName').value;
+    user.cellPhone = document.getElementById('txtCellPhone').value; 
+    user.adress = document.getElementById('txtAdress').value;
+    user.mail = document.getElementById('txtMail').value;
+    user.password = document.getElementById('txtPassword').value;
+    user.type = document.getElementById('txtType').value;
+    console.log(user);
+
+    if(user.password == document.getElementById('txtRepeat').value){
+        const request = await fetch('http://localhost:8080/api/user/register', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        });
+        window.location.replace('index.html');
+    } else {
+        alert('Las contraseñas no coinciden, por favor retifique');
+    }
+
+}
+
