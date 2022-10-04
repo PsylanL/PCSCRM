d31f59efd647e517c0be2e0a3a30a121326a01ab
Juan David Uribe <juan_uribe82201@elpoli.edu.co>
2022-10-04 14:29:55 -0500

    (FIX)fixed show name in homePage

--- FrontEnd/js/Header.js
+++ FrontEnd/js/Header.js
@@ -1,11 +1,30 @@
-let userHTML = document.getElementById('user');
-
 let mail = localStorage.email;
-
-userHTML.innerHTML = ' <i class="icon-users" ></i>' + mail;
+let user = {} ;
 
 function cerrarSesion() {
     localStorage.token = null;
     localStorage.email = null;
+    localStorage.id = null;
     window.location.replace('index.html');
 }
+
+async function getIdByEmail (){
+    const getUsers = "http://localhost:8080/api/user/list";
+    let request = await fetch(getUsers);
+    let response = await request.json();
+    response.forEach(element => {
+        if (element.mail == mail){
+            localStorage.id = element.id;
+        }
+    });
+    getUser();
+}
+
+async function getUser (){
+    const getUser = 'http://localhost:8080/api/user/search/'+localStorage.id;
+    let request = await fetch(getUser);
+    user = await request.json();
+    let userHTML = document.getElementById('user');
+    userHTML.innerHTML = ' <i class="icon-users" ></i>' + user[0].name;
+}
+window.onload = getIdByEmail(); 
