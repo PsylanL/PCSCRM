adf56695dc0909e31fc83799f1288c3cafb00b65
psylan <dylan_lopez82201@elpoli.edu.co>
2022-10-03 19:09:27 -0500

    (FIX) Fixed test branch because of merge bug

--- FrontEnd/js/clients.js
+++ FrontEnd/js/clients.js
@@ -66,17 +66,35 @@ async function deleteClient(id){
 }
 window.onload = client; 
 const openModal = document.getElementById('btn-agg-client');
+const  openModal2 = document.querySelector('.register_button');
 const  modal1 = document.querySelector('.modal1');
+const  modal2 = document.querySelector('.modal2');
+
+//const  modal2 = document.querySelector('.modal2');
 const closeModal = document.querySelector('.modal_close');
+const closeModal2 = document.getElementById('modal_close2');
 openModal.addEventListener('click', (e)=>{ 
     e.preventDefault();/*Evita que se ponga un # en el url cada que se abre el modal */
     modal1.classList.add('modal1--show')
+})
+
+openModal2.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal2.classList.add('modal2--show')
+    modal1.classList.remove('modal1--show')
 })
 
 closeModal.addEventListener('click', (e)=>{ 
     e.preventDefault();/*Evita que se ponga un # en el url cada que se abre el modal */
     modal1.classList.remove('modal1--show')
 })
+
+closeModal2.addEventListener('click', (e)=>{ 
+    e.preventDefault();/*Evita que se ponga un # en el url cada que se abre el modal */
+    modal2.classList.remove('modal2--show')
+    modal1.classList.add('modal1--show')
+})
+
 
 window.onload = client; 
 
